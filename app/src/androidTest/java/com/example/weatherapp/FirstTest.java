18bf2fd9c28fae1ab7a097de969b4dbc509b8698
Pivotal <pivotal@piv-wifi-17-172.pivotallabs.com>
2016-01-25 15:31:20 -0500

    up to task 2

--- app/src/androidTest/java/com/example/weatherapp/FirstTest.java
+++ app/src/androidTest/java/com/example/weatherapp/FirstTest.java
@@ -14,7 +14,9 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.Espresso.openContextualActionModeOverflowMenu;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -44,5 +46,16 @@ public class FirstTest {
         onView(allOf(withId(R.id.title), withText("Settings"))).perform(click());
         onView(allOf(withId(android.R.id.title), withText("Forecast Preference"))).check(matches(withText("Forecast Preference")));
         SystemClock.sleep(1000);
+    }
+
+    @Test
+    public void task2()
+    {
+        openContextualActionModeOverflowMenu();
+        onView(allOf(withId(R.id.title), withText("Settings"))).perform(click());
+        onView(allOf(withId(android.R.id.title), withText("Set location"))).perform(click());
+        onView(withId(android.R.id.edit)).perform(clearText(), typeText("Waterloo,Canada"));
+        onView(withId(android.R.id.button1)).perform(click());
+        onView(allOf(withId(android.R.id.summary), withText("Waterloo,Canada")));
     }
 }
